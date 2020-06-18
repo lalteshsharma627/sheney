38537caabee56a5ad8da64ddb5767359fb15df80
Laltesh Sharma <lalteshsharma1997@gmail.com>
2020-06-18 12:56:12 +0530

    first

---
+++ css/header.php
@@ -0,0 +1,35 @@
+<div class="header-top">
+	  			<div class="container">
+			  		<div class="row">
+			  			<div class="col-lg-6 col-sm-6 col-8 header-top-left no-padding">
+			  				<ul>
+								<li><a href="https://www.facebook.com/ChameleonClasses"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="https://www.twitter.com/chameleon_class"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="https://www.instagram.com/ChameleonClasses"><i class="fa fa-instagram"></i></a></li>
+								<li><a href="https://www.youtube.com/c/ChameleonClasses?sub_confirmation=1"><i class="fa fa-youtube"></i></a></li>
+			  				</ul>			
+			  			</div>
+			  			<div class="col-lg-6 col-sm-6 col-4 header-top-right no-padding">
+			  				<a href="tel:971 1514 157"><i class="fa fa-phone"></i></a>
+							  <a href="mailto:chameleonclasses@gmail.com"><i class="fa fa-envelope"></i></a>	
+							  <a href="#"  style="padding:0px;"><i class="fa fa-user-plus" ></i></a>	
+			  			</div>
+			  		</div>			  					
+	  			</div>
+			</div>
+		    <div class="container main-menu">
+		    	<div class="row align-items-center justify-content-between d-flex">
+			      <div id="logo">
+			        <img src="img/logo.png" alt="" title="" style="width:100px; height:60px;"/>
+			      </div>
+			      <nav id="nav-menu-container">
+			        <ul class="nav-menu">
+			          <li><a href="index.htm">Home</a></li>
+			          <li><a href="about.htm">About</a></li>
+			          <li><a href="courses.html">Courses</a></li>
+					  <li><a href="event-details.html">Event Details</a></li>	
+			          
+			          
+			      </nav><!-- #nav-menu-container -->		    		
+		    	</div>
+		    </div>
